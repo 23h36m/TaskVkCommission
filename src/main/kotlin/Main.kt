bd57a6de20b359d937f079a4e443094410febca2
Daniil Migurin <ubrandnw@gmail.com>
2024-03-20 01:18:05 +0300

    UpDate

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,10 +6,16 @@ fun main() {
     val commissionPercentage = 0.0075
 
     val scanner = Scanner(System.`in`)
+    println("Введите систему перевода (Карта или VK Pay)")
+    val system = scanner.nextLine()
     println("Введите сумму перевода ")
-    val amount = scanner.nextInt()
+    val amount = Integer.parseInt(scanner.nextLine())
 
-    val commission = if (amount*commissionPercentage > minCommission)
-       amount*commissionPercentage else minCommission
-    println("Сумма комиссии = $commission рублей")
+    if (system == "Карта") {
+        val commission = if (amount * commissionPercentage > minCommission)
+            amount * commissionPercentage else minCommission
+        println("Сумма комиссии = $commission рублей")
+    } else {
+        println("Сумма комиссии = 0 рублей")
+    }
 }
